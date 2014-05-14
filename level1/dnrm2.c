5417a5f596f85ad4a7332041bc94907dfbbc7f21
lehn <michael.lehn@uni-ulm.de>
2014-05-14 15:57:02 +0200

    Suggested solution for proper port of dnrm2.f to ulmBLAS

--- level1/dnrm2.c
+++ level1/dnrm2.c
@@ -1,7 +1,42 @@
+#include <math.h>
+
 double
-dnrm2_(const int     *n,
+dnrm2_(const int     *_n,
        const double  *x,
-       const int     *incX)
+       const int     *_incX)
 {
-    return 0.0;
+//
+//  Dereference scalar parameters
+//
+    int n    = *_n;
+    int incX = *_incX;
+
+//
+//  Local scalars
+//
+    int    i;
+    double result, scale, ssq, absX;
+
+    if (n<1 || incX<1) {
+        result = 0.0;
+    } else if (n==1) {
+        result = fabs(x[0]);
+    } else {
+        scale = 0.0;
+        ssq   = 1.0;
+        for (i=0; i<1+(n-1)*incX; i+=incX) {
+            if (x[i]!=0.0) {
+                absX = fabs(x[i]);
+                if (scale<absX) {
+                    ssq = 1.0 + ssq*pow(scale/absX, 2.0);
+                    scale = absX;
+                } else {
+                    ssq += pow(absX/scale, 2.0);
+                }
+            }
+        }
+        result = scale*sqrt(ssq);
+    }
+
+    return result;
 }
