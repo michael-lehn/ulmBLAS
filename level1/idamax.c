959a4e3d52d56386b037b9f63b115e953a04d105
lehn <michael.lehn@uni-ulm.de>
2014-05-14 17:44:30 +0200

    Suggested solution for proper port of idamax.f to ulmBLAS.

--- level1/idamax.c
+++ level1/idamax.c
@@ -1,7 +1,54 @@
+#include <math.h>
+
 int
-idamax_(const int       *n,
+idamax_(const int       *_n,
         const double    *x,
-        const int       *incX)
+        const int       *_incX)
 {
-    return 1;
+//
+//  Dereference scalar parameters
+//
+    int n    = *_n;
+    int incX = *_incX;
+
+//
+//  Local scalars
+//
+    int    i, iamax;
+    double amax;
+
+//
+//  Quick return if possible
+//
+    if (n<1 || incX<=0) {
+        return 0;
+    }
+    if (n==1) {
+        return 1;
+    }
+    if (incX==1) {
+//
+//      Code for increment equal to 1
+//
+        amax = fabs(x[0]);
+        for (i=1; i<n; ++i) {
+            if (fabs(x[i])>amax) {
+                iamax = i;
+                amax  = fabs(x[i]);
+            }
+        }
+    } else {
+//
+//      Code for increment not equal to 1
+//
+        iamax = 0;
+        amax  = fabs(x[0]);
+        for (i=1, x+=incX; i<n; ++i, x+=incX) {
+            if (fabs(*x)>amax) {
+                iamax = i;
+                amax  = fabs(*x);
+            }
+        }
+    }
+    return iamax+1;
 }
