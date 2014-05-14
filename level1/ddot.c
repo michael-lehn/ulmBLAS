fe006bc88d730c818cc63fea25b5940b1c2935a9
lehn <michael.lehn@uni-ulm.de>
2014-05-14 14:37:49 +0200

    Suggested solution for proper port of ddot.f to ulmBLAS

--- level1/ddot.c
+++ level1/ddot.c
@@ -5,22 +5,57 @@ ddot_(const int     *_n,
       const double  *y,
       const int     *_incY)
 {
+//
+//  Dereference scalar parameters
+//
     int n    = *_n;
     int incX = *_incX;
     int incY = *_incY;
 
-    int    i;
+//
+//  Local scalars
+//
     double result = 0.0;
 
-    if (incX<0) {
-        x -= incX*(n-1);
-    }
-    if (incY<0) {
-        y -= incY*(n-1);
-    }
-    for (i=0; i<n; ++i, x+=incX, y+=incY) {
-        result += (*x) * (*y);
-    }
+    int    i, m;
 
+//
+//  Quick return if possible
+//
+    if (n==0) {
+        return 0;
+    }
+    if (incX==1 && incY==1) {
+//
+//      Code for both increments equal to 1
+//
+        m = n % 5;
+        for (i=0; i<m; ++i) {
+            result += x[i] * y[i];
+        }
+        if (n<5) {
+            return result;
+        }
+        for (i=m; i<n; i+=5) {
+            result += x[i  ] * y[i  ] ;
+            result += x[i+1] * y[i+1];
+            result += x[i+2] * y[i+2];
+            result += x[i+3] * y[i+3];
+            result += x[i+4] * y[i+4];
+        }
+    } else {
+//
+//      Code for unequal increments or equal increments not equal to 1
+//
+        if (incX<0) {
+            x -= incX*(n-1);
+        }
+        if (incY<0) {
+            y -= incY*(n-1);
+        }
+        for (i=0; i<n; ++i, x+=incX, y+=incY) {
+            result += (*x) * (*y);
+        }
+    }
     return result;
 }
