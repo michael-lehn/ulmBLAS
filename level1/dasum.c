9bd2d06cbed30d617691d75daa7b9d9520352d7b
lehn <michael.lehn@uni-ulm.de>
2014-05-14 15:02:01 +0200

    Suggested solution for proper port of dasum.f to ulmBLAS

--- level1/dasum.c
+++ level1/dasum.c
@@ -1,7 +1,60 @@
+#include <math.h>
+
 double
-dasum_(const int    *n,
+dasum_(const int    *_n,
        const double *x,
-       const int    *incX)
+       const int    *_incX)
 {
-    return 0.0;
+//
+//  Dereference scalar parameters
+//
+    int n    = *_n;
+    int incX = *_incX;
+
+//
+//  Local scalars
+//
+    double result = 0.0;
+    int    i, m;
+
+//
+//  Quick return if possible
+//
+    if (n<=0 || incX<=0) {
+        return result;
+    }
+    if (incX==1) {
+//
+//      Code for increment equal to 1
+//
+        m = n % 6;
+        if (m!=0) {
+            for (i=0; i<m; ++i) {
+                result += fabs(x[i]);
+            }
+            if (n<6) {
+                return result;
+            }
+        }
+        for (i=m; i<n; i+=6) {
+            result += fabs(x[i  ]);
+            result += fabs(x[i+1]);
+            result += fabs(x[i+2]);
+            result += fabs(x[i+3]);
+            result += fabs(x[i+4]);
+            result += fabs(x[i+5]);
+        }
+    } else {
+//
+//      Code for increment not equal to 1
+//
+        if (incX<0) {
+            x -= incX*(n-1);
+        }
+        for (i=0; i<n; ++i, x+=incX) {
+            result += fabs(*x);
+        }
+    }
+
+    return result;
 }
