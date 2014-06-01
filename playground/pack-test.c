f93c5031c18e74f06df06432b6a40e0fc9f29585
lehn <michael.lehn@uni-ulm.de>
2014-06-01 22:49:34 +0200

    Packing of a MC x KC matrix block in A to buffer. With zero padding.

--- playground/pack-test.c
+++ playground/pack-test.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #define M   14
@@ -39,11 +40,11 @@ printMatrix(int m, int n, double *X, int ldX)
 }
 
 void
-pack_MRxk(int n, double  *A, int incRowA, int incColA, double *buffer)
+pack_MRxk(int k, double  *A, int incRowA, int incColA, double *buffer)
 {
     int i, j;
 
-    for (j=0; j<n; ++j) {
+    for (j=0; j<k; ++j) {
         for (i=0; i<MR; ++i) {
             buffer[i] = A[i*incRowA];
         }
@@ -52,22 +53,59 @@ pack_MRxk(int n, double  *A, int incRowA, int incColA, double *buffer)
     }
 }
 
+void
+pack_A(int mc, int kc, double *A, int incRowA, int incColA, double *buffer)
+{
+    const int MP  = mc / MR;
+    const int _MR = mc % MR;
+
+    int i, j, I;
+
+    assert(mc<=MC);
+    assert(kc<=KC);
+
+    for (I=0; I<MP; ++I) {
+        pack_MRxk(kc, A, incRowA, incColA, buffer);
+        A      += MR;
+        buffer += MR*kc;
+    }
+    if (_MR>0) {
+        for (j=0; j<kc; ++j) {
+            for (i=0; i<_MR; ++i) {
+                buffer[i] = A[i];
+            }
+            for (i=_MR; i<MR; ++i) {
+                buffer[i] = 0.0;
+            }
+            A      += incColA;
+            buffer += MR;
+        }
+    }
+}
+
 int
 main()
 {
+    int i, j, mc, kc;
+
     initMatrix(M, K, A, M, 1);
 
     printf("A = \n");
     printMatrix(M, K, A, M);
 
-    pack_MRxk(KC, A, 1, M, _A);
 
-    printf("Buffer: _A = \n");
-    printMatrix(MC, KC, _A, MC);
+    for (j=0; j<K; j+=KC) {
+        kc = (j+KC<=K) ? KC : K - j;
 
-    pack_MRxk(KC, A+MR, 1, M, _A+MR*KC);
+        for (i=0; i<M; i+=MC) {
+            mc = (i+MC<=M) ? MC : M - i;
 
-    printf("Buffer: _A = \n");
-    printMatrix(MC, KC, _A, MC);
+            printf("Packing A(%d:%d, %d:%d)\n", i+1, i+mc, j+1, j+kc);
+            pack_A(mc, kc, &A[i+j*M], 1, M, _A);
+
+            printf("Buffer: _A = \n");
+            printMatrix(MC, KC, _A, MC);
+        }
+    }
     return 0;
 }
