c6b14523988f4b5f4a1b7b9b70c8ec80f2b94d0e
Client Admin <clientadmin@Schalk.local>
2014-07-04 14:26:44 +0200

    Bugfix

--- src/level3/dgemm_nn.h
+++ src/level3/dgemm_nn.h
@@ -2,19 +2,19 @@
 #define LEVEL3_DGEMM_NN_H 1
 
 void
-ULMBLAS(dgemm_nn)(int            m,
-                  int            n,
-                  int            k,
-                  double         alpha,
-                  const double   *A,
-                  int            incRowA,
-                  int            incColA,
-                  const double   *B,
-                  int            incRowB,
-                  int            incColB,
-                  double         beta,
-                  double         *C,
-                  int            incRowC,
-                  int            incColC);
+dgemm_nn(int            m,
+         int            n,
+         int            k,
+         double         alpha,
+         const double   *A,
+         int            incRowA,
+         int            incColA,
+         const double   *B,
+         int            incRowB,
+         int            incColB,
+         double         beta,
+         double         *C,
+         int            incRowC,
+         int            incColC);
 
 #endif // LEVEL3_DGEMM_NN_H
