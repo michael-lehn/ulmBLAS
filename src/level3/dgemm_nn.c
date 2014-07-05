7867e7135d4830b94fb400749d8f2eb82684696a
Client Admin <clientadmin@Schalk.local>
2014-07-06 01:56:18 +0200

    Converted the intrinsic attempt into inline assembler.  As the asm block is declared volatile the compiler can not reorder instructions.

--- src/level3/dgemm_nn.c
+++ src/level3/dgemm_nn.c
@@ -124,95 +124,132 @@ dgemm_micro_kernel(long kc,
                    double beta,
                    double *C, long incRowC, long incColC)
 {
-    double AB[MR*NR] __attribute__ ((aligned (16)));
+    double _AB[MR*NR] __attribute__ ((aligned (16)));
+    double *AB = _AB;
 
-    int i, j, l;
+    int i, j;
 
 //
 //  Compute AB = A*B
 //
-    register __m128d ab_00_11, ab_20_31;
-    register __m128d ab_01_10, ab_21_30;
-    register __m128d ab_02_13, ab_22_33;
-    register __m128d ab_03_12, ab_23_32;
-
-    register __m128d tmp0, tmp1, tmp2, tmp3;
-    register __m128d tmp4, tmp5, tmp6, tmp7;
-
-    tmp0     = _mm_load_pd(A);                                      // (1)
-    tmp1     = _mm_load_pd(A+2);                                    // (2)
-    tmp2     = _mm_load_pd(B);                                      // (3)
-
-    ab_00_11 = _mm_setzero_pd(); ab_20_31 = _mm_setzero_pd();
-    ab_01_10 = _mm_setzero_pd(); ab_21_30 = _mm_setzero_pd();
-    ab_02_13 = _mm_setzero_pd(); ab_22_33 = _mm_setzero_pd();
-    ab_03_12 = _mm_setzero_pd(); ab_23_32 = _mm_setzero_pd();
-
-    tmp3 = _mm_setzero_pd();
-    tmp4 = _mm_setzero_pd();
-    tmp5 = _mm_setzero_pd();
-    tmp6 = _mm_setzero_pd();
-    tmp7 = _mm_setzero_pd();
-
-    for (l=0; l<kc; ++l) {
-        ab_02_13 = _mm_add_pd(ab_02_13, tmp3);                      // (9)
-        tmp3     = _mm_load_pd(B+2);
-        ab_22_33 = _mm_add_pd(ab_22_33, tmp6);                      // (10)
-        tmp6     = tmp2;
-        tmp4     = _mm_shuffle_pd(tmp2, tmp2, _MM_SHUFFLE2(0, 1));  // (8)
-        tmp2     = _mm_mul_pd(tmp2, tmp0);
-        tmp6     = _mm_mul_pd(tmp6, tmp1);
-
-        ab_03_12 = _mm_add_pd(ab_03_12, tmp5);                      // (15)
-        ab_23_32 = _mm_add_pd(ab_23_32, tmp7);                      // (16)
-        tmp7     = tmp4;
-        tmp4     = _mm_mul_pd(tmp4, tmp0);
-        tmp7     = _mm_mul_pd(tmp7, tmp1);
-
-        ab_00_11 = _mm_add_pd(ab_00_11, tmp2);                      // (11)
-        tmp2     = _mm_load_pd(B+4);                                // (6)
-        ab_20_31 = _mm_add_pd(ab_20_31, tmp6);                      // (12)
-        tmp6     = tmp3;
-        tmp5     = _mm_shuffle_pd(tmp3, tmp3, _MM_SHUFFLE2(0, 1));  // (7)
-        tmp3     = _mm_mul_pd(tmp3, tmp0);
-        tmp6     = _mm_mul_pd(tmp6, tmp1);
-
-        ab_01_10 = _mm_add_pd(ab_01_10, tmp4);                      // (13)
-        ab_21_30 = _mm_add_pd(ab_21_30, tmp7);                      // (14)
-        tmp7     = tmp5;
-        tmp5     = _mm_mul_pd(tmp5, tmp0);
-        tmp0     = _mm_load_pd(A+4);                                // (4)
-        tmp7     = _mm_mul_pd(tmp7, tmp1);
-        tmp1     = _mm_load_pd(A+6);                                // (5)
-
-        A += 4;
-        B += 4;
-    }
-    ab_02_13 = _mm_add_pd(ab_02_13, tmp3);                          // (9)
-    ab_22_33 = _mm_add_pd(ab_22_33, tmp6);                          // (10)
-
-    ab_03_12 = _mm_add_pd(ab_03_12, tmp5);                          // (15)
-    ab_23_32 = _mm_add_pd(ab_23_32, tmp7);                          // (16)
-
-    _mm_storel_pd(&AB[0+0*4], ab_00_11);
-    _mm_storeh_pd(&AB[1+0*4], ab_01_10);
-    _mm_storel_pd(&AB[2+0*4], ab_20_31);
-    _mm_storeh_pd(&AB[3+0*4], ab_21_30);
-
-    _mm_storel_pd(&AB[0+1*4], ab_01_10);
-    _mm_storeh_pd(&AB[1+1*4], ab_00_11);
-    _mm_storel_pd(&AB[2+1*4], ab_21_30);
-    _mm_storeh_pd(&AB[3+1*4], ab_20_31);
-
-    _mm_storel_pd(&AB[0+2*4], ab_02_13);
-    _mm_storeh_pd(&AB[1+2*4], ab_03_12);
-    _mm_storel_pd(&AB[2+2*4], ab_22_33);
-    _mm_storeh_pd(&AB[3+2*4], ab_23_32);
-
-    _mm_storel_pd(&AB[0+3*4], ab_03_12);
-    _mm_storeh_pd(&AB[1+3*4], ab_02_13);
-    _mm_storel_pd(&AB[2+3*4], ab_23_32);
-    _mm_storeh_pd(&AB[3+3*4], ab_22_33);
+    __asm__ volatile
+    (
+    "movl      %0,      %%esi    \n\t"  // kc (32 bit) stored in %esi
+    "movq      %1,      %%rax    \n\t"  // Address of A stored in %rax
+    "movq      %2,      %%rbx    \n\t"  // Address of B stored in %rbx
+    "movq      %3,      %%rcx    \n\t"  // Address of AB stored in %rcx
+    "                            \n\t"
+    "movaps    (%%rax), %%xmm0   \n\t"  // tmp0 = _mm_load_pd(A)
+    "movaps  16(%%rax), %%xmm1   \n\t"  // tmp1 = _mm_load_pd(A+2)
+    "movaps    (%%rbx), %%xmm2   \n\t"  // tmp2 = _mm_load_pd(B)
+    "                            \n\t"
+    "xorpd     %%xmm8,  %%xmm8   \n\t"  // ab_00_11 = _mm_setzero_pd()
+    "xorpd     %%xmm9,  %%xmm9   \n\t"  // ab_20_31 = _mm_setzero_pd()
+    "xorpd     %%xmm10, %%xmm10  \n\t"  // ab_01_10 = _mm_setzero_pd()
+    "xorpd     %%xmm11, %%xmm11  \n\t"  // ab_21_30 = _mm_setzero_pd()
+    "xorpd     %%xmm12, %%xmm12  \n\t"  // ab_02_13 = _mm_setzero_pd()
+    "xorpd     %%xmm13, %%xmm13  \n\t"  // ab_22_33 = _mm_setzero_pd()
+    "xorpd     %%xmm14, %%xmm14  \n\t"  // ab_03_12 = _mm_setzero_pd()
+    "xorpd     %%xmm15, %%xmm15  \n\t"  // ab_23_32 = _mm_setzero_pd()
+    "                            \n\t"
+    "xorpd     %%xmm3,  %%xmm3   \n\t"  // tmp3 = _mm_setzero_pd
+    "xorpd     %%xmm4,  %%xmm4   \n\t"  // tmp4 = _mm_setzero_pd
+    "xorpd     %%xmm5,  %%xmm5   \n\t"  // tmp5 = _mm_setzero_pd
+    "xorpd     %%xmm6,  %%xmm6   \n\t"  // tmp6 = _mm_setzero_pd
+    "xorpd     %%xmm7,  %%xmm7   \n\t"  // tmp7 = _mm_setzero_pd
+    "                            \n\t"
+    "testl     %%esi,   %%esi    \n\t"  // if kc==0 start writeback to AB
+    "je        .DWRITEBACK%=     \n\t"
+    "                            \n\t"
+    ".DLOOP%=:                   \n\t"  // for l = kc,..,1 do
+    "                            \n\t"
+    "addpd     %%xmm3,  %%xmm12  \n\t"  // ab_02_13 = _mm_add_pd(ab_02_13, tmp3)
+    "movapd  16(%%rbx), %%xmm3   \n\t"  // tmp3     = _mm_load_pd(B+2)
+    "addpd     %%xmm6,  %%xmm13  \n\t"  // ab_22_33 = _mm_add_pd(ab_22_33, tmp6)
+    "movapd    %%xmm2,  %%xmm6   \n\t"  // tmp6     = tmp2
+    "pshufd $78,%%xmm2, %%xmm4   \n\t"  // tmp4     = _mm_shuffle_pd(tmp2, tmp2,
+    "                            \n\t"  //                   _MM_SHUFFLE2(0, 1))
+    "mulpd     %%xmm0,  %%xmm2   \n\t"  // tmp2     = _mm_mul_pd(tmp2, tmp0);
+    "mulpd     %%xmm1,  %%xmm6   \n\t"  // tmp6     = _mm_mul_pd(tmp6, tmp1);
+    "                            \n\t"
+    "                            \n\t"
+    "addpd     %%xmm5,  %%xmm14  \n\t"  // ab_03_12 = _mm_add_pd(ab_03_12, tmp5)
+    "addpd     %%xmm7,  %%xmm15  \n\t"  // ab_23_32 = _mm_add_pd(ab_23_32, tmp7)
+    "movapd    %%xmm4,  %%xmm7   \n\t"  // tmp7     = tmp4
+    "mulpd     %%xmm0,  %%xmm4   \n\t"  // tmp4     = _mm_mul_pd(tmp4, tmp0)
+    "mulpd     %%xmm1,  %%xmm7   \n\t"  // tmp7     = _mm_mul_pd(tmp7, tmp1)
+    "                            \n\t"
+    "                            \n\t"
+    "addpd     %%xmm2,  %%xmm8   \n\t"  // ab_00_11 = _mm_add_pd(ab_00_11, tmp2)
+    "movapd  32(%%rbx), %%xmm2   \n\t"  // tmp2     = _mm_load_pd(B+4)
+    "addpd     %%xmm6,  %%xmm9   \n\t"  // ab_20_31 = _mm_add_pd(ab_20_31, tmp6)
+    "movapd    %%xmm3,  %%xmm6   \n\t"  // tmp6     = tmp3
+    "pshufd $78,%%xmm3, %%xmm5   \n\t"  // tmp5     = _mm_shuffle_pd(tmp3, tmp3,
+    "                            \n\t"  //                   _MM_SHUFFLE2(0, 1))
+    "mulpd     %%xmm0,  %%xmm3   \n\t"  // tmp3     = _mm_mul_pd(tmp3, tmp0)
+    "mulpd     %%xmm1,  %%xmm6   \n\t"  // tmp6     = _mm_mul_pd(tmp6, tmp1)
+    "                            \n\t"
+    "                            \n\t"
+    "addpd     %%xmm4,  %%xmm10  \n\t"  // ab_01_10 = _mm_add_pd(ab_01_10, tmp4)
+    "addpd     %%xmm7,  %%xmm11  \n\t"  // ab_21_30 = _mm_add_pd(ab_21_30, tmp7)
+    "movapd    %%xmm5,  %%xmm7   \n\t"  // tmp7     = tmp5
+    "mulpd     %%xmm0,  %%xmm5   \n\t"  // tmp5     = _mm_mul_pd(tmp5, tmp0)
+    "movapd  32(%%rax), %%xmm0   \n\t"  // tmp0     = _mm_load_pd(A+4)
+    "mulpd     %%xmm1,  %%xmm7   \n\t"  // tmp7     = _mm_mul_pd(tmp7, tmp1)
+    "movapd  48(%%rax), %%xmm1   \n\t"  // tmp1     = _mm_load_pd(A+6)
+    "                            \n\t"
+    "                            \n\t"
+    "addq      $32,     %%rax    \n\t"  // A += 4;
+    "addq      $32,     %%rbx    \n\t"  // B += 4;
+    "                            \n\t"
+    "decl      %%esi             \n\t"  // --l
+    "jne       .DLOOP%=          \n\t"  // if l>= 1 go back
+    "                            \n\t"
+    "                            \n\t"
+    "addpd    %%xmm3,   %%xmm12  \n\t"  // ab_02_13 = _mm_add_pd(ab_02_13, tmp3)
+    "addpd    %%xmm6,   %%xmm13  \n\t"  // ab_22_33 = _mm_add_pd(ab_22_33, tmp6)
+    "                            \n\t"  //
+    "addpd    %%xmm5,   %%xmm14  \n\t"  // ab_03_12 = _mm_add_pd(ab_03_12, tmp5)
+    "addpd    %%xmm7,   %%xmm15  \n\t"  // ab_23_32 = _mm_add_pd(ab_23_32, tmp7)
+    "                            \n\t"
+    ".DWRITEBACK%=:              \n\t"  // Fill AB with computed values
+    "                            \n\t"
+    "                            \n\t"  // Fill first col of AB:
+    "movlpd   %%xmm8,    (%%rcx) \n\t"  // _mm_storel_pd(&AB[0+0*4], ab_00_11)
+    "movhpd   %%xmm10 , 8(%%rcx) \n\t"  // _mm_storeh_pd(&AB[1+0*4], ab_01_10)
+    "movlpd   %%xmm9,  16(%%rcx) \n\t"  // _mm_storel_pd(&AB[2+0*4], ab_20_31)
+    "movhpd   %%xmm11, 24(%%rcx) \n\t"  // _mm_storeh_pd(&AB[3+0*4], ab_21_30)
+    "                            \n\t"
+    "addq     $32,       %%rcx   \n\t"  // Fill second col of AB:
+    "movlpd   %%xmm10,   (%%rcx) \n\t"  // _mm_storel_pd(&AB[0+1*4], ab_01_10)
+    "movhpd   %%xmm8,   8(%%rcx) \n\t"  // _mm_storeh_pd(&AB[1+1*4], ab_00_11)
+    "movlpd   %%xmm11, 16(%%rcx) \n\t"  // _mm_storel_pd(&AB[2+1*4], ab_21_30)
+    "movhpd   %%xmm9,  24(%%rcx) \n\t"  // _mm_storeh_pd(&AB[3+1*4], ab_20_31)
+    "                            \n\t"
+    "addq     $32,       %%rcx   \n\t"  // Fill third col of AB:
+    "movlpd   %%xmm12,   (%%rcx) \n\t"  // _mm_storel_pd(&AB[0+2*4], ab_02_13)
+    "movhpd   %%xmm14,  8(%%rcx) \n\t"  // _mm_storeh_pd(&AB[1+2*4], ab_03_12)
+    "movlpd   %%xmm13, 16(%%rcx) \n\t"  // _mm_storel_pd(&AB[2+2*4], ab_22_33)
+    "movhpd   %%xmm15, 24(%%rcx) \n\t"  // _mm_storeh_pd(&AB[3+2*4], ab_23_32)
+    "                            \n\t"
+    "addq     $32,       %%rcx   \n\t"  // Fill forth col of AB:
+    "movlpd   %%xmm14,   (%%rcx) \n\t"  // _mm_storel_pd(&AB[0+3*4], ab_03_12)
+    "movhpd   %%xmm12,  8(%%rcx) \n\t"  // _mm_storeh_pd(&AB[1+3*4], ab_02_13)
+    "movlpd   %%xmm15, 16(%%rcx) \n\t"  // _mm_storel_pd(&AB[2+3*4], ab_23_32)
+    "movhpd   %%xmm13, 24(%%rcx) \n\t"  // _mm_storeh_pd(&AB[3+3*4], ab_22_33)
+    : // output
+    : // input
+        "m" (kc),     // 0
+        "m" (A),      // 1
+        "m" (B),      // 2
+        "m" (AB)      // 3
+    : // register clobber list
+        "rax", "rbx", "rcx", "esi",
+        "xmm0", "xmm1", "xmm2", "xmm3",
+        "xmm4", "xmm5", "xmm6", "xmm7",
+        "xmm8", "xmm9", "xmm10", "xmm11",
+        "xmm12", "xmm13", "xmm14", "xmm15"
+    );
 
 //
 //  Update C <- beta*C
