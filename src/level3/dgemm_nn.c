503c912106f37f6830babb0d95a58df60193b206
Michael Lehn <michael.lehn@uni-ulm.de>
2014-07-10 09:10:48 +0200

    Removed the prefetching (we add this later!)

--- src/level3/dgemm_nn.c
+++ src/level3/dgemm_nn.c
@@ -169,8 +169,6 @@ dgemm_micro_kernel(int kc,
     "                            \n\t"
     ".DLOOP%=:                   \n\t"  // for l = kb,..,1 do
     "                            \n\t"
-    "prefetcht0  (4*35+1+16) * 8(%%rax) \n\t"
-    "                            \n\t"  // 1. update
     "addpd     %%xmm3,  %%xmm12  \n\t"  // ab_02_13 = _mm_add_pd(ab_02_13, tmp3)
     "movaps  16(%%rbx), %%xmm3   \n\t"  // tmp3     = _mm_load_pd(B+2)
     "addpd     %%xmm6,  %%xmm13  \n\t"  // ab_22_33 = _mm_add_pd(ab_22_33, tmp6)
@@ -244,8 +242,6 @@ dgemm_micro_kernel(int kc,
     "mulpd     %%xmm1,  %%xmm7   \n\t"  // tmp7     = _mm_mul_pd(tmp7, tmp1)
     "movaps  80(%%rax), %%xmm1   \n\t"  // tmp1     = _mm_load_pd(A+10)
     "                            \n\t"
-    "                            \n\t"
-    "prefetcht0  (4*37+1+16) * 8(%%rax) \n\t"
     "                            \n\t"
     "                            \n\t"  // 3. update
     "addpd     %%xmm3,  %%xmm12  \n\t"  // ab_02_13 = _mm_add_pd(ab_02_13, tmp3)
